37062be8a33ad2daa80502c3f8a9f3a572ffa891
DMYC <dreammakersysc@gmail.com>
2026-01-03 01:02:17 +0530

    Delete admin directory

--- admin/js/supabase.js
+++
@@ -1,8 +0,0 @@
-// js/supabase.js
-// Supabase Client එක සෑදීම
-
-const supabaseUrl = 'https://tnrqasiiywcvtwkwytqe.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRucnFhc2lpeXdjdnR3a3d5dHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNDIxMTEsImV4cCI6MjA4MjkxODExMX0.rd9aKmLa687zMxg8FV5AllgnYkz7wpdXBRi3a5NyXc4';
-
-// window.supabaseClient ලෙස ගෝලීයව සකසයි
-const _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
